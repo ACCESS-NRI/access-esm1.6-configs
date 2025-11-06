fc74aea23b41658ebd9a53c1004d9344675f3eb9
Srbinovsky, Jhan (O&A, Aspendale) <Jhan.Srbinovsky@csiro.au>
2025-11-07 10:19:39 +1100

    update config to test new crop parameters

---
+++ copy_nmls.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp --remove-destination pftlookup_cable3_OctB.csv /scratch/p66/jxs599/access-esm/work/test-PFTs-access-esm1.6-configs-257-test-revised-pft-parameters-d7984826/atmosphere/INPUT/pftlookup_cable3_1213_exp6_c4crop.csv 
+
