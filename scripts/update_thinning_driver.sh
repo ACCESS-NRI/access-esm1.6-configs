7f83705c0dd59fccb8575e4d0b916c64c2e44423
Whyborn <lachlan.s.whyborn@gmail.com>
2025-12-01 10:28:08 +1100

    Try to get the script to run

--- scripts/update_thinning_driver.sh
+++ scripts/update_thinning_driver.sh
@@ -4,5 +4,5 @@ set -eu
 
 current_rst=atmosphere/restart_dump.astart
 
-python ./scripts/update_thinning.py --restart-file="./atmosphere/restart_dump.astart \
-  --thinning-file=/g/data/p66/tfl561/thinning/harvest_frac/cable_thinning_frac_1850-2015.nc
+python ./scripts/update_thinning.py --restart-file="./atmosphere/restart_dump.astart" \
+  --thinning-file="/g/data/p66/tfl561/thinning/harvest_frac/cable_thinning_frac_1850-2015.nc"
