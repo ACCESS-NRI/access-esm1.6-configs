c592c867806fbd97b73ed0d911b2d65d6fa3ae5d
anton-seaice <anton.steketee@anu.edu.au>
2026-01-13 14:28:27 +1100

    tidy

--- documentation/hooks/hide_pages_url_segment.py
+++ documentation/hooks/hide_pages_url_segment.py
@@ -5,7 +5,7 @@ from mkdocs.plugins import event_priority
 def on_page_markdown(markdown, *, page, config, files):
     if page.file.url.startswith("pages/"):
         # Remove "pages/" prefix from the URL
-        # page.file.url = page.file.url.removeprefix("pages/")
+        page.file.url = page.file.url.removeprefix("pages/")
         page.file.dest_uri = page.file.dest_uri.removeprefix("pages/")
-        # page.file.abs_dest_path = page.file.abs_dest_path.removeprefix("pages/")
+        page.file.abs_dest_path = page.file.abs_dest_path.removeprefix("pages/")
         page.canonical_url = config.site_url + page.file.url
