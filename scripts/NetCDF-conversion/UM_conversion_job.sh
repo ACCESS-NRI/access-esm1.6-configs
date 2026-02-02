300cb64e5ae5decb6ebf89bc78d5bb0454b618a9
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2026-02-02 16:44:42 +1100

    Add addmeta processing to um2nc postscript job
     - Daily and monthly atmosphere command files with hardcoded realm and frequency
     - Daily and monthly ice command files with hardcoded realm and frequency
     - Fixed and time-vaying ocean command files with inferred frequency and realm logic
    
    Added in validation for checking, included schema data so checks can run in a PBS job

--- scripts/NetCDF-conversion/UM_conversion_job.sh
+++ scripts/NetCDF-conversion/UM_conversion_job.sh
@@ -2,15 +2,34 @@
 #PBS -l ncpus=1
 #PBS -l mem=20GB
 #PBS -l jobfs=0GB
-#PBS -q normal
+#PBS -q express
 #PBS -l walltime=01:30:00
 #PBS -l wd
+#PBS -l storage=gdata/vk83
 #PBS -W umask=027
 
-module use /g/data/vk83/modules
-module load payu
+module use /g/data/vk83/prerelease/modules
+module load payu/dev
+
+module list
+which payu
+which addmeta
 
 # Command below calls https://github.com/ACCESS-NRI/um2nc-standalone/blob/main/umpost/conversion_driver_esm1p5.py
 # By default UM atmosphere fields files are deleted after conversion to save space. 
 # Remove --delete-ff command line option to retain original files for testing purposes
 esm1p5_convert_nc $PAYU_CURRENT_OUTPUT_DIR --delete-ff
+
+# Clean up global metatdata to meet ACCESS-NRI dataspec standards
+# https://access-output-data-specifications--2.org.readthedocs.build/en/2/
+for cmdfile in addmeta/{ocean,ice,atmosphere}/addmeta*.args
+do
+  addmeta -v -c ${cmdfile}
+done
+
+# Validate
+for f in  ${PAYU_CURRENT_OUTPUT_DIR}/{ocean,ice,atmosphere/netCDF}/*.nc
+do
+  echo $f
+  validatemeta -v -s testing/2-0-0/2-0-0.json $f
+done
