8606dd2d3fd1e1825503a354248a82e95b2bc2aa
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2026-02-02 16:44:42 +1100

    Add addmeta processing to um2nc postscript job

--- scripts/NetCDF-conversion/UM_conversion_job.sh
+++ scripts/NetCDF-conversion/UM_conversion_job.sh
@@ -14,3 +14,12 @@ module load payu
 # By default UM atmosphere fields files are deleted after conversion to save space. 
 # Remove --delete-ff command line option to retain original files for testing purposes
 esm1p5_convert_nc $PAYU_CURRENT_OUTPUT_DIR --delete-ff
+
+# Clean up global metatdata to meet ACCESS-NRI dataspec standards
+# https://access-output-data-specifications--2.org.readthedocs.build/en/2/
+addmeta -v -c ocean/addmeta.args
+addmeta -v -c atmosphere/addmeta.args
+addmeta -v -c ice/addmeta.args
+
+# Validate
+validatemeta -s testing/data-spec-2-0-0.json ${$PAYU_CURRENT_OUTPUT_DIR}/{ocean|ice|atmosphere}/*.nc
