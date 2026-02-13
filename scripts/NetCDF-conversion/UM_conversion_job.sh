376eaa5a5e43d3c9532b1a6216658e3bbd30e4ef
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2026-02-13 17:46:41 +1100

    Uncomment um2nc in postscript

--- scripts/NetCDF-conversion/UM_conversion_job.sh
+++ scripts/NetCDF-conversion/UM_conversion_job.sh
@@ -18,7 +18,7 @@ which addmeta
 # Command below calls https://github.com/ACCESS-NRI/um2nc-standalone/blob/main/umpost/conversion_driver_esm1p5.py
 # By default UM atmosphere fields files are deleted after conversion to save space. 
 # Remove --delete-ff command line option to retain original files for testing purposes
-# esm1p5_convert_nc $PAYU_CURRENT_OUTPUT_DIR --delete-ff
+esm1p5_convert_nc $PAYU_CURRENT_OUTPUT_DIR --delete-ff
 
 # Clean up global metatdata to meet ACCESS-NRI dataspec standards
 # https://access-output-data-specifications--2.org.readthedocs.build/en/2/
