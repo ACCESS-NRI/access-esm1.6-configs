d3de81ab61a6e37d6ea08b984c3c2ac64cf1b6fb
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2026-03-05 16:45:26 +1100

    Add command line argument option to schema_check.sh

--- scripts/NetCDF-conversion/schema_check.sh
+++ scripts/NetCDF-conversion/schema_check.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-module use /g/data/vk83/prerelease/modules
-module load payu/dev
+# Run validate meta on all files in specified payu outputXXX directory
 
-module list
-which payu
-which validatemeta
+show_usage() {
+    echo "Usage: $(basename "$0") <output_directory_name>" >&2
+    echo "Error: Missing required argument <output_directory_name>." >&2
+    exit 1
+}
+
+# Check if the number of arguments ($#) is less than 1
+if [[ $# -lt 1 ]]; then
+    show_usage
+fi
 
 ACCESS_OUTPUT_SCHEMA_URL="https://raw.githubusercontent.com/ACCESS-NRI/schema/refs/heads/main/au.org.access-nri/model/output/file-metadata/2-0-0/2-0-0.json"
 
 # Validate
-for f in  ${PAYU_CURRENT_OUTPUT_DIR}/{ocean,ice,atmosphere/netCDF}/*.nc
+for f in  $1/{ocean,ice,atmosphere/netCDF}/*.nc
 do
   echo $f
   validatemeta -v -s ${ACCESS_OUTPUT_SCHEMA_URL} $f
