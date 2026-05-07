255f9d115e1cd373e3fe9a519030392e2904ac9e
Spencer Wong <spencer.wong@anu.edu.au>
2026-05-08 09:30:41 +1000

    Add wood thinning script and input

---
+++ scripts/update_thinning_driver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -eu
+
+./scripts/update_thinning.py --restart-file="./work/atmosphere/restart_dump.astart" \
+  --thinning-file="./work/atmosphere/INPUT/LUH3_cable_thinning_frac_from_bioh_1850-2023_v2.nc"
