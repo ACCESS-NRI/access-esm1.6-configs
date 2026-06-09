65069180e2a09cf2c6f3fee21b5dede654d41d25
Spencer Wong <spencer.wong@anu.edu.au>
2026-06-10 09:16:22 +1000

    Add missing argument for thinning driver

--- scripts/update_thinning_driver.sh
+++ scripts/update_thinning_driver.sh
@@ -3,4 +3,5 @@
 set -eu
 
 ./scripts/update_thinning.py --restart-file="./work/atmosphere/restart_dump.astart" \
-  --thinning-file="./work/atmosphere/INPUT/LUH3_cable_thinning_frac_from_bioh_1850-2023_v2.nc"
+  --thinning-file="./work/atmosphere/INPUT/LUH3_cable_thinning_frac_from_bioh_1850-2023_v2.nc" \
+  --stashmaster-file "./work/atmosphere/INPUT/STASHmaster/STASHmaster_A"
