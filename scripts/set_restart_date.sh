a947f593693ee1b6f28ed317d654a67143096e55
Spencer Wong <spencer.wong@anu.edu.au>
2026-06-09 14:53:10 +1000

    Add scripts to modify the initial restart for a historical simulation:
    Change the restart year to 1850
    Insert an initial wood product of 20PgC
    Insert the initial wood thinning data

---
+++ scripts/set_restart_date.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This script sets the start date in an ESM1.6 restart file to the specified year, month and day
+# See https://forum.access-hive.org.au/t/change-the-date-in-an-esm1-6-restart/5615
+
+set -eu
+
+while getopts ":r:y:m:d:" opt; do
+    case ${opt} in
+        r)
+            RESTARTDIR=${OPTARG}
+        ;;
+        y)
+            YEAR=${OPTARG}
+            printf -v zyear "%04d" $YEAR
+        ;;
+        m)
+            MONTH=${OPTARG}
+            printf -v zmonth "%02d" $MONTH
+        ;;
+        d)
+            DAY=${OPTARG}
+            printf -v zday "%02d" $DAY
+        ;;
+        :)
+            echo "Option \"-${OPTARG}\" requires an argument." 
+            exit 1 
+        ;;
+    esac
+done
+
+
+#### Atmosphere ####
+python scripts/change_dump_date.py $RESTARTDIR/atmosphere/restart_dump.astart -o $RESTARTDIR/atmosphere/new_restart -y $YEAR -m $MONTH -d $DAY
+mv $RESTARTDIR/atmosphere/new_restart $RESTARTDIR/atmosphere/restart_dump.astart
+
+echo "end_date: $zyear-$zmonth-$zday 00:00:00" > $RESTARTDIR/atmosphere/um.res.yaml
+
+
+#### Ocean ####
+sed -i "s/.*Current model time:.*/$YEAR     $MONTH     $DAY     0     0     0        Current model time: year, month, day, hour, minute, second/" $RESTARTDIR/ocean/ocean_solo.res
+
+
+#### Ice ####
+iced_file="iced.${zyear}-${zmonth}-${zday}-00000.nc"
+
+mv $RESTARTDIR/ice/iced.????-??-??-00000.nc $RESTARTDIR/ice/$iced_file
+
+echo "./RESTART/$iced_file" > $RESTARTDIR/ice/ice.restart_file
+
+ncatted -O -a year,global,o,l,$YEAR $RESTARTDIR/ice/$iced_file
+ncatted -O -a nyr,global,o,l,$YEAR $RESTARTDIR/ice/$iced_file
+ncatted -O -a month,global,o,l,$MONTH $RESTARTDIR/ice/$iced_file
+ncatted -O -a mday,global,o,l,$DAY $RESTARTDIR/ice/$iced_file
+
+# Calculate total seconds from 1/1/1 to specified date
+seconds=$( python scripts/calc_seconds.py -y $YEAR -m $MONTH -d $DAY)
+ncatted -O -a time,global,o,d,$seconds $RESTARTDIR/ice/$iced_file
---
+++ scripts/setup_historical_restart.py
@@ -0,0 +1,105 @@
+import argparse
+import git
+from ruamel.yaml import YAML
+from pathlib import Path
+import shutil
+import shlex
+import subprocess
+
+import insert_external_woodprod_CNP
+import update_thinning
+
+# Filepaths required for the initial wood product script
+EXTERNAL_NC_PATH = "/g/data/vk83/prerelease/configurations/inputs/access-esm1p6/modern/historical/atmosphere/land/biogeochemistry/global.N96/2026.05.08/access-esm16_wood_prod_initial_1850_20PgC.nc"
+STASHMASTER_BASE_PATH = "/g/data/vk83/prerelease/configurations/inputs/access-esm1p6/share/atmosphere/stash/2026.01.21/STASHmaster/STASHmaster_A"
+STASHMASTER_EXT_PATH = "./atmosphere/prefix.PRESM_A"
+
+# Filepaths required for wood thinning
+THINNING_FILE = "/g/data/vk83/configurations/inputs/access-esm1p6/modern/historical/atmosphere/land/vegetation/global.N96/2026.05.08/LUH3_cable_thinning_frac_from_bioh_1850-2023_v2.nc"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=(
+            "Copy and modify an ESM1.6 restart directory for use as the"
+            "initial condition in a historical simulation. This includes:\n"
+            " - Resetting the restart date to 1850-01-01\n"
+            " - Inserting an initial wood product of 20PgC into the atmosphere restart\n"
+            "The initial restart path is taken from the config.yaml, and the output restart path is specified as an argument."
+        ),
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        '--output-restart',
+        help='Path for saving modified output ESM1.6 restart directory.',
+        type=Path,
+        required=True
+        )
+
+    return parser.parse_args()
+
+
+def copy_restart(input, output):
+    """Copy the input restart directory to the specified output location"""
+    if output.exists():
+        raise FileExistsError(f"Output path {output} already exists.")
+
+    print(f"Copying {input} to {output}")
+    shutil.copytree(input, output)
+
+
+def set_restart_date(restart_dir, year, month, day):
+    """Set the date for each component's restart files"""
+    print(f"Setting restart date to {year:04d}-{month:02d}-{day:02d}")
+    cmd = f"./scripts/set_restart_date.sh -r {restart_dir} -y {year} -m {month} -d {day}"
+    subprocess.check_call(shlex.split(cmd))
+
+
+def update_config(restart_dir, config):
+    """Update the restart path in the config.yaml"""
+    print("Updating config.yaml with new restart path")
+    config["restart"] = str(restart_dir)
+    YAML().dump(config, Path("config.yaml"))
+
+
+def commit_config(input_restart, output_restart):
+    """Commit changes to the config.yaml"""
+    repo = git.Repo(".")
+    repo.index.add("config.yaml")
+    msg = (
+        f"Restarts in {input_restart} copied to {output_restart} and modified\n"
+        f"using {Path(__file__).name}\n"
+        " * Restart date changed to 1850 01 01.\n"
+        " * Initial wood product inserted into atmosphere restart."
+    )
+    print(f"Commiting changes to config.yaml with message: '{msg}'")
+    repo.index.commit(msg)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    config = YAML().load(Path("config.yaml"))
+    input_restart = config["restart"]
+    copy_restart(input_restart, args.output_restart)
+
+    # Set restart date to 1850 01 01
+    set_restart_date(args.output_restart, year=1850, month=1, day=1)
+
+    # Insert initial wood product into atmosphere restart
+    atm_restart = args.output_restart/"atmosphere"/"restart_dump.astart"
+    insert_external_woodprod_CNP.insert_woodprod(
+        restart_path=str(atm_restart),
+        external_nc_path=EXTERNAL_NC_PATH,
+        stashmaster_base_path=STASHMASTER_BASE_PATH,
+        stashmaster_ext_path=STASHMASTER_EXT_PATH,
+        output_path=str(atm_restart)
+    )
+
+    # Add initial wood thinning data to restart
+    update_thinning.insert_thinning(
+        restart_file=str(atm_restart),
+        thinning_file=THINNING_FILE,
+        stashmaster_file=STASHMASTER_BASE_PATH
+    )
+    update_config(args.output_restart, config)
+    commit_config(input_restart, args.output_restart)
