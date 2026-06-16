6789a7af01b4e14e514b77db24af1355076d4d47
Spencer Wong <spencer.wong@anu.edu.au>
2026-06-17 09:48:16 +1000

    Apply wood thinning at end of the run

--- scripts/update_thinning.py
+++ scripts/update_thinning.py
@@ -3,7 +3,6 @@
 import um_replace_field
 import xarray
 import argparse
-import mule
 import tempfile
 import shutil
 
@@ -24,15 +23,20 @@ def _parse_args():
         required=True,
         help='Wood thinning file to use as source.'
         )
+    parser.add_argument(
+        '--stashmaster-file',
+        type=str,
+        required=True,
+        help='Path to base STASHMaster_A file.'
+        )
 
     return parser.parse_args()
 
-if __name__ == "__main__":
-    args = _parse_args()
 
+def insert_thinning(restart_file, thinning_file, stashmaster_file):
     um_file = um_replace_field.open_fields_file(
-        args.restart_file,
-        "work/atmosphere/INPUT/STASHmaster/STASHmaster_A",
+        restart_file,
+        stashmaster_file,
         0
         )
 
@@ -40,7 +44,7 @@ if __name__ == "__main__":
     model_year = um_file.fixed_length_header.t2_year
     t_index = model_year - ref_year
 
-    thinning_file = xarray.open_dataset(args.thinning_file, decode_times=False)
+    thinning_file = xarray.open_dataset(thinning_file, decode_times=False)
 
     # Create a temporary file to write to
     tmp = tempfile.NamedTemporaryFile()
@@ -53,5 +57,9 @@ if __name__ == "__main__":
         t_index
         )
 
-    shutil.move(args.restart_file, args.restart_file + "_orig")
-    shutil.copy(tmp.name, args.restart_file)
+    shutil.copy(tmp.name, restart_file)
+
+
+if __name__ == "__main__":
+    args = _parse_args()
+    insert_thinning(args.restart_file, args.thinning_file, args.stashmaster_file)
