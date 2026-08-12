0c2446550e2364d93924acfe7a8330ca40af4fb5
Spencer Wong <spencer.wong@anu.edu.au>
2026-08-12 15:27:22 +1000

    Add splitnc for sea ice outputs. Add addmeta

---
+++ scripts/post-processing/post_process.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#PBS -l ncpus=1
+#PBS -l mem=60GB
+#PBS -l jobfs=0GB
+#PBS -q normal
+#PBS -l walltime=02:30:00
+#PBS -l wd
+#PBS -W umask=027
+
+set -e
+
+module use /g/data/vk83/staging/modules
+module load model-processing/20260805T125219-7abd909-pr90
+
+# Convert UM atmosphere fields files to netCDF.
+# By default UM atmosphere fields files are deleted after conversion to save space. 
+# Remove --delete-ff command line option to retain original files for testing purposes
+# Source code for the um2nc command is available at https://github.com/ACCESS-NRI/um2nc-standalone/
+um2nc driver esm1p6 $PAYU_CURRENT_OUTPUT_DIR --delete-ff  --one-nc-per-stash-variable
+
+
+# Split sea ice output into single variable files using the splitnc command. The splitnc command calls https://github.com/ACCESS-NRI/splitnc/blob/main/src/splitnc/splitnc.py
+icefiles=($PAYU_CURRENT_OUTPUT_DIR/ice/iceh-*)
+if [ "${#icefiles[@]}" -gt 0 ]
+then
+    splitnc --shared-vars uarea,tmask,tarea --excluded-vars VGRD.  --use-esm1p6-filenames --fix-cell-methods --overwrite "${icefiles[@]}"
+
+    # Remove the original sea ice model output:
+    rm "${icefiles[@]}"
+fi
+
+
+# Clean up global metatdata to meet ACCESS-NRI dataspec standards
+# https://access-output-data-specifications--2.org.readthedocs.build/en/2/
+
+# Atmosphere
+addmeta \
+    -v -s \
+    -d metadata.yaml \
+    -d $PAYU_CURRENT_OUTPUT_DIR/env.yaml \
+    -m scripts/post-processing/addmeta/dataspec.yaml \
+    -m scripts/post-processing/addmeta/atmosphere.yaml \
+    --fnregex='access-esm1p6\.\w+(?:\.\dd)?\.(?P<var>\w+)\.(?P<freq>\w{2,4})(?:\.\w+)?(?:\.\d{4})?\.nc' \
+    $PAYU_CURRENT_OUTPUT_DIR/atmosphere/*.nc
+
+# Sea ice
+addmeta \
+    -v -s \
+    -d metadata.yaml \
+    -d $PAYU_CURRENT_OUTPUT_DIR/env.yaml \
+    -m scripts/post-processing/addmeta/dataspec.yaml \
+    -m scripts/post-processing/addmeta/ice.yaml \
+    --fnregex='access-esm1p6\.\w+(?:\.\dd)?\.(?P<var>\w+)\.(?P<freq>\w{2,4})(?:\.\w+)?(?:\.\d{4})?\.nc' \
+    $PAYU_CURRENT_OUTPUT_DIR/ice/*.nc
+
+# Ocean
+addmeta \
+    -v -s \
+    -m scripts/post-processing/addmeta/realm_only.yaml \
+    --datavar realm='ocean' \
+    $PAYU_CURRENT_OUTPUT_DIR/ocean/ocean.*.nc
+
+addmeta \
+    -v -s \
+    -m scripts/post-processing/addmeta/realm_only.yaml \
+    --datavar freq='ocnBgchem' \
+    $PAYU_CURRENT_OUTPUT_DIR/ocean/ocnBgchem.*.nc
+
+# Rename ocean files to remove the realm and "_" in the date
+python3 scripts/post-processing/rename_ocean.py $PAYU_CURRENT_OUTPUT_DIR/ocean/{ocean,ocnBgchem}.*.nc
+
+
+addmeta \
+    -v -s \
+    -d metadata.yaml \
+    -d $PAYU_CURRENT_OUTPUT_DIR/env.yaml \
+    -m scripts/post-processing/addmeta/dataspec.yaml \
+    -m scripts/post-processing/addmeta/ocean.yaml \
+    --fnregex='access-esm1p6\.\w+(?:\.\dd)?\.(?P<var>\w+)\.(?P<freq>\w{2,4})(?:\.\w+)?(?:\.\d{4})?\.nc' \
+    $PAYU_CURRENT_OUTPUT_DIR/ocean/*.nc
