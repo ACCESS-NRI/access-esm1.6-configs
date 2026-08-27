b8e23ddb436610ebc25954bc580b00da1d81afac
Spencer Wong <88933912+blimlim@users.noreply.github.com>
2026-08-27 16:00:37 +1000

    Post processing for data specification (#1003)
    * Add post processing for data specification compliant output  (#313, #950)
    
    * Swap to yearly output files for atmosphere
    
    * Add updated um2nc in model-processing environment for single variable atmosphere output
    
    * Run addmeta to inject metadata into the netCDF outputs
    
    * Use data spec compliant output file names
    
    Co-authored-by: Aidan Heerdegen <aidan.heerdegen@gmail.com>
    
    Co-authored-by: joshuatorrance <joshuatorrance@gmail.com>
    
    * Remove ocean and ice post processing
    
    * Set base_configuration for amip
    
    * Fix loop in post process script
    
    Co-authored-by: joshuatorrance <joshuatorrance@gmail.com>
    
    ---------
    
    Co-authored-by: joshuatorrance <joshuatorrance@gmail.com>

---
+++ scripts/post-processing/post_process.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#PBS -l ncpus=1
+#PBS -l mem=60GB
+#PBS -l jobfs=0GB
+#PBS -q normal
+#PBS -l walltime=02:30:00
+#PBS -l wd
+#PBS -W umask=027
+
+set -e
+
+module use /g/data/vk83/modules
+module load model-processing/1.0.0_0
+
+# Convert UM atmosphere fields files to netCDF.
+# By default UM atmosphere fields files are deleted after conversion to save space. 
+# Remove --delete-ff command line option to retain original files for testing purposes
+# Source code for the um2nc command is available at https://github.com/ACCESS-NRI/um2nc-standalone/
+um2nc driver esm1p6 $PAYU_CURRENT_OUTPUT_DIR --delete-ff  --one-nc-per-stash-variable
+
+
+# Clean up global metatdata to meet ACCESS-NRI dataspec standards
+# https://access-output-data-specifications.readthedocs.io/en/latest/specification/
+
+for submodel in atmosphere;
+do
+    addmeta \
+        -v -s \
+        -d metadata.yaml \
+        -d $PAYU_CURRENT_OUTPUT_DIR/env.yaml \
+        -m scripts/post-processing/addmeta/dataspec.yaml \
+        -m scripts/post-processing/addmeta/${submodel}.yaml \
+        --fnregex='access-esm1p6\.\w+(?:\.\dd)?\.(?P<var>\w+)\.(?P<freq>\w{2,4})(?:\.\w+)?(?:\.\d{4})?\.nc' \
+        $PAYU_CURRENT_OUTPUT_DIR/${submodel}/*.nc
+done
